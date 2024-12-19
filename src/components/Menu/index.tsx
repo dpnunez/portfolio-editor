5e06b67c992509a5fc25169d6ef6f8dc1a8bc957
Daniel Núñez <daniel.portonunez@gmail..com>
2024-12-19 20:45:19 -0300

    feat(menu): create menu structure

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { MenuItem } from "./Item";
+import { useCallback } from "react";
+
+function Menu() {
+  const pathname = usePathname();
+  const isRouteActive = useCallback(
+    (route: string) => pathname.includes(route),
+    [pathname]
+  );
+
+  return (
+    <nav className="h-full divide-x-1 flex max-h-16">
+      <MenuItem active={isRouteActive("/about")} href="/about">
+        About
+      </MenuItem>
+      <MenuItem active={isRouteActive("/projects")} href="/projects">
+        Projects
+      </MenuItem>
+    </nav>
+  );
+}
+
+export { Menu };
