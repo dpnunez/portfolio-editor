fcf39580bf9a8a29d74b6ea28b9fbbe4ecf09408
Daniel Núñez <daniel.portonunez@gmail..com>
2024-12-19 14:33:01 -0300

    feat(dx): add initial tooling to start development
    add linter, formatter, semantic versioning and semantic releasing

---
+++ release.config.mjs
@@ -0,0 +1,30 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+const config = {
+  branches: [
+    { name: "main" },
+    {
+      name: "alpha",
+      prerelease: "alpha",
+    },
+  ],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { breaking: true, release: "minor" }, // Breaking changes are minor during alpha
+          { revert: true, release: "patch" },
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "perf", release: "patch" },
+        ],
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
+
+export default config;
