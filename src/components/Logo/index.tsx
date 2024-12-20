a754c3ccdaf9ef8e215221dbe6bc05f72f3389ba
Daniel Núñez <daniel.portonunez@gmail.com>
2024-12-20 10:15:51 -0300

    refactor(logo): remove useless tag

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,16 +2,17 @@ import Link from "next/link";
 
 function Logo() {
   return (
-    <div className="w-sidebar flex justify-center border-r border-editor-divider h-full items-center">
-      <Link href="/">
-        <h1 className="text-lg group">
-          <span className="text-editor-primary font-bold">whoami</span>
-          <span className="inline-block w-28 text-center opacity-60 italic group-hover:opacity-100 group-hover:text-editor-secondary group-hover:font-bold transition-all">
-            {"  >  "}dpnunez
-          </span>
-        </h1>
-      </Link>
-    </div>
+    <Link
+      href="/"
+      className="w-sidebar flex justify-center border-r border-editor-divider h-full items-center group"
+    >
+      <h1 className="text-lg">
+        <span className="text-editor-primary font-bold">whoami</span>
+        <span className="inline-block w-28 text-center opacity-60 italic group-hover:opacity-100 group-hover:text-editor-secondary group-hover:font-bold transition-all">
+          {"  >  "}dpnunez
+        </span>
+      </h1>
+    </Link>
   );
 }
 
