b48285b33be732c8678466b95707a74c2e9b19df
Daniel Núñez <daniel.portonunez@gmail.com>
2024-12-20 20:59:37 -0300

    feat(state management): create useStateViaQueryParam hook

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,62 @@
-function Page() {
-  return null;
-}
+"use client";
+import { useCallback } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+  Checkbox,
+  Sidebar,
+  TreeRowContainer,
+} from "@/components";
+import { projectTags } from "@/constants/filters";
+import { useStateViaQueryParams } from "@/hooks";
 
-export default Page;
+export default function Page() {
+  const [search, setSearch] = useStateViaQueryParams<string[]>("search", []);
+
+  const handleSearch = useCallback(
+    (checked: boolean, id: string) => {
+      setSearch((prev) => {
+        if (checked) {
+          return [...prev, id];
+        }
+        return prev.filter((e) => e !== id);
+      });
+    },
+    [setSearch]
+  );
+
+  return (
+    <>
+      <Sidebar>
+        <Accordion type="single" collapsible defaultValue="open">
+          <AccordionItem value="open">
+            <AccordionTrigger className="text-md p-4 [&[data-state=open]]:bg-editor-divider">
+              File Explorer
+            </AccordionTrigger>
+            <AccordionContent className="py-4">
+              {projectTags.map((e) => {
+                const isChecked = search.includes(e);
+                return (
+                  <label key={e} className="cursor-pointer">
+                    <TreeRowContainer key={e} className="items-center">
+                      <div className="w-6 flex items-center">
+                        <Checkbox
+                          checked={isChecked}
+                          onCheckedChange={(value) => handleSearch(!!value, e)}
+                        />
+                      </div>
+                      <span>{e}</span>
+                    </TreeRowContainer>
+                  </label>
+                );
+              })}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </Sidebar>
+      <main>AA</main>
+    </>
+  );
+}
---
+++ src/hooks/useStateViaQueryParams.ts
@@ -0,0 +1,31 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+const useStateViaQueryParams = <T extends string | string[]>(
+  key: string,
+  defaultValue: T
+): [T, Dispatch<SetStateAction<T>>] => {
+  const router = useRouter();
+  const [state, setState] = useState<T>(() => {
+    const value = new URLSearchParams(window.location.search).get(key);
+    if (Array.isArray(defaultValue)) {
+      return (value ? value.split(",") : defaultValue) as T;
+    }
+    return (value || defaultValue) as T;
+  });
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const newState = Array.isArray(state) ? state.join(",") : (state as string);
+    url.searchParams.set(key, newState);
+    if (!newState || newState?.length === 0) {
+      url.searchParams.delete(key);
+    }
+    router.push(url.toString());
+  }, [key, state, router]);
+
+  return [state, setState];
+};
+
+export { useStateViaQueryParams };
