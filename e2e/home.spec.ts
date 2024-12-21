7260a4c2e5be422d669f56ea8a5c0f740b2b1a3e
Daniel Núñez <daniel.portonunez@gmail.com>
2024-12-21 19:15:02 -0300

    test(home): add e2e test for home page

---
+++ e2e/home.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+
+test("Home Page", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+  await expect(page.getByText("Hi all, I'm")).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Daniel Núñez" })
+  ).toBeVisible();
+  await expect(page.getByText("Full Stack Developer,")).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "// fell free to access the" })
+  ).toBeVisible();
+});
