782b4f0f32b4ba313ff7a94c8c79838477dd7827
Daniel Núñez <daniel.portonunez@gmail.com>
2024-12-25 22:59:35 -0300

    feat(contact form): register fields on RHF

--- src/app/contact/form.tsx
+++ src/app/contact/form.tsx
@@ -1,9 +1,64 @@
 "use client";
 
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+} from "@/components";
+import { useForm } from "react-hook-form";
+
 function ContactForm() {
+  const form = useForm();
+
   return (
     <div>
-      <form></form>
+      <Form {...form}>
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="John Doe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="john.doe@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Input type="text" placeholder="Your message here" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </Form>
     </div>
   );
 }
