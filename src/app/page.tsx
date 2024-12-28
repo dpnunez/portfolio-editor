1406ca89c69e93c83b9c26a68419bc33090db5d2
Daniel Núñez <daniel.portonunez@gmail.com>
2024-12-28 12:48:01 -0300

    feat(home): improve home mobile styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ import { PrismLight } from "react-syntax-highlighter";
 function Page() {
   return (
     <FadeIn className="overflow-hidden flex-1 flex items-center relative">
-      <div className="gap-4 flex flex-col max-w-screen-md mx-auto flex-1">
+      <div className="gap-4 flex flex-col max-w-screen-md mx-auto flex-1 px-4 items-center md:items-start">
         <span>Hi all, I&apos;m</span>
         <h1 className="font-extrabold text-3xl md:text-6xl">Daniel Núñez</h1>
-        <h2 className="text-lg mb-4">
+        <h2 className="text-lg mb-4 text-center md:text-left">
           <span>{"> "}</span>
           <span>
             Full Stack Developer, specialized in Javascript environment
