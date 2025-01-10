d97bc620372df5549cd18ce75fd880a3cb72cca5
Daniel Núñez <daniel.portonunez@gmail.com>
2025-01-10 20:44:15 -0300

    ci(release generator): remove "development stage" semantic release rules

--- release.config.mjs
+++ release.config.mjs
@@ -4,18 +4,7 @@
 const config = {
   branches: [{ name: "main" }],
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        releaseRules: [
-          { breaking: true, release: "minor" }, // Breaking changes are minor during alpha
-          { revert: true, release: "patch" },
-          { type: "feat", release: "minor" },
-          { type: "fix", release: "patch" },
-          { type: "perf", release: "patch" },
-        ],
-      },
-    ],
+    "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
   ],
